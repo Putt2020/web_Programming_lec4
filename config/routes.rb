bfd6bfcc10e20bc50870bf6c840f257d67705014
Putt2020 <6230067421@student.chula.ac.th>
2021-09-09 17:31:13 +0700

    inclass exercise done but is it correct? KEKW

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  
   get '/users/destroy', to: 'users#destroy'
   get '/users/Create_fast', to: 'users#revive'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
